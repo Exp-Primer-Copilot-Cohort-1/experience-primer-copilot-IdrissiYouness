c643f6e57b45e26d2466bc4295c48ae79b3054a4
Youness Sghiouri Idrissi <108573203+IdrissiYouness@users.noreply.github.com>
2023-10-26 13:14:15 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,15 @@
+/**
+ * Adds two numbers together.
+ * @param {number} num1 - The first number to add.
+ * @param {number} num2 - The second number to add.
+ * @returns {number} The sum of num1 and num2.
+ */
+// Create web server
+
+function addNumbers(num1, num2) {
+    return num1 + num2;
+  }
+
+  
+  let result = addNumbers(5, 10);
+  console.log(result); // Output: 15
